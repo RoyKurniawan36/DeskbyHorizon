d3ab90523f45a19296420448533413f55f429e54
RoyKurniawan36 <kurniawan.2205541146@student.unud.ac.id>
2026-01-05 03:54:58 +0800

    Initial commit - Desk by Horizon E-Commerece

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         $middleware->web(append: [
             \App\Http\Middleware\SetLocale::class,
         ]);
+        
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
